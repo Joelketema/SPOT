ebfe244da4658a2f0f1b74f068413c8a53206374
Eyuel Ketema <eyuelket2019@gmail.com>
2022-03-12 19:51:26 -0800

    first day

---
+++ src/Components/Authenticate/Auth.js
@@ -0,0 +1,14 @@
+
+import Register from '../Registration/Register'
+import Login from '../Login/Login'
+
+const Auth = () => {
+    return (
+        <div className="wrapper">
+            <Register />
+            <Login/>
+        </div>
+    )
+}
+
+export default Auth;
